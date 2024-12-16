7184a6e4fbae596247151a768d0a182dd194ab20
Benjy Smith <newzealandbenjy@gmail.com>
2024-12-17 10:08:08 +1300

    Add an example to show incorrect result when Phib is changing (due to B0 changing), but there is no transport

---
+++ torax/examples/moving_surfaces_no_transport.py
@@ -0,0 +1,77 @@
+"""
+Configuration to demonstrate unphysical results when flux surfaces move.
+If the number density and temperature are initially set to be constant across rhon,
+and all transport and sources are disabled, the solution should remain constant,
+even if the flux surfaces move. However, this example demonstrates that varying B0 with time
+causes a noticeable effect on the solution, which is unphysical.
+"""
+
+
+CONFIG = {
+    'runtime_params': {
+        'profile_conditions': {
+            'set_pedestal': False,
+            # A constant inital Ti, Te and ne
+            'Ti': {0: 15.0, 1: 15.0},
+            'Te': {0: 15.0, 1: 15.0},
+            'ne': {0: 1.5, 1: 1.5},
+        },
+        'plasma_composition': {},
+        'numerics': {
+            'ion_heat_eq': True,
+            'el_heat_eq': True,
+            'current_eq': False,
+            'dens_eq': True,
+            # Disabling Phibdot calculation will leave Ti, Te and ne fixed,
+            #  demonstrating that the erronous effect is due to Phibdot
+            # 'calcphibdot': False,
+        },
+    },
+    'geometry': {
+        'geometry_type': 'circular',
+        'n_rho': 25,
+        'geometry_configs': {
+          0.0: {
+            'B0': 1.0,
+          },
+          1.0: {
+            'B0': 2.0,
+          },
+          2.0: {
+            'B0': 1.0,
+          }
+        }
+    },
+    # No sources
+    'sources': {
+        'j_bootstrap': {'mode': 'zero'},
+        'generic_current_source': {'mode': 'zero'},
+        'generic_particle_source': {'mode': 'zero'},
+        'gas_puff_source': {'mode': 'zero'},
+        'pellet_source': {'mode': 'zero'},
+        'generic_ion_el_heat_source': {'mode': 'zero'},
+        'fusion_heat_source': {'mode': 'zero'},
+        'qei_source': {'mode': 'zero'},
+        'ohmic_heat_source': {'mode': 'zero'},
+    },
+    # No transport
+    'transport': {
+        'transport_model': 'constant',
+        'constant_params': {
+            'chii_const': 0.0,
+            'chie_const': 0.0,
+            'De_const': 0.0,
+            'Ve_const': 0.0,
+            'chimin': 0.0,
+            'Demin': 0.0,
+            'Vemin': 0.0,
+        },
+    },
+    # Simple stepper and time step calculator
+    'stepper': {
+        'stepper_type': 'linear',
+    },
+    'time_step_calculator': {
+        'calculator_type': 'fixed',
+    },
+}
